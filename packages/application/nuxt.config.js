4c2dff7c9111244e25e78c5adfbac120b0f17143
Sebastian Guillema <sguillema@hotmail.com>
2018-08-22 23:12:10 +1000

    Rebuilt application with Vue CLI 3 and vuetify/nuxt template

--- packages/application/nuxt.config.js
+++ packages/application/nuxt.config.js
@@ -1,3 +1,6 @@
+const nodeExternals = require('webpack-node-externals')
+const resolve = (dir) => require('path').join(__dirname, dir)
+
 module.exports = {
   /*
   ** Headers of the page
@@ -10,9 +13,14 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Web Application for 41094, Wrk1 01 1.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    '~/assets/style/app.styl'
+  ],
   /*
   ** Customize the progress bar color
   */
@@ -21,11 +29,25 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    babel: {
+      plugins: [
+        ["transform-imports", {
+          "vuetify": {
+            "transform": "vuetify/es5/components/${member}",
+            "preventFullImport": true
+          }
+        }]
+      ]
+    },
+    vendor: [
+      '~/plugins/vuetify.js'
+    ],
+    extractCSS: true,
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
@@ -33,7 +55,13 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
     }
   }
 }
-
