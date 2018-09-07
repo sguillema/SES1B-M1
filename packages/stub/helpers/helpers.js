23e64f2dc03fa0c638cfc976c56de15d2685bbec
Sebastian Guillema <sguillema@hotmail.com>
2018-09-07 17:43:16 +1000

    Documented helpers

--- packages/stub/helpers/helpers.js
+++ packages/stub/helpers/helpers.js
@@ -1,12 +1,22 @@
 const moment = require('moment')
 
 class Helpers {
+    /**
+     * Get a random number between two provided number limits
+     * @param {number} min The lower limit to generate a value from
+     * @param {number} max The upper limit to generate a value from
+     * @returns {number} A random number
+     */
     getRandomInt(min, max){
         min = Math.ceil(min)
         max = Math.floor(max)
         return Math.floor(Math.random() * (max - min)) + min
     }
 
+    /**
+     * Generate a uid using the current time, with a random number appended to it
+     * @returns {string} A uid
+     */
     generateId(){
         return moment().format("x") + this.getRandomInt(1000, 9999).toString()
     }
