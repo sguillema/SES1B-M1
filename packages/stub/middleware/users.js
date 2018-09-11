10b6c4c10e6410cf2b1aadefaab9bfed21e99758
Sebastian Guillema <sguillema@hotmail.com>
2018-09-11 10:57:15 +1000

    Added authentication endpoint for users.
    Todo: Implement token and session authentication/management.

--- packages/stub/middleware/users.js
+++ packages/stub/middleware/users.js
@@ -22,7 +22,7 @@ users.post('/users', async (req, res) => {
     req.body.uid = helpers.generateId()
     req.body.created_at = moment().format()
 
-    let password =  middleware.setPassword(req.body.password)
+    let password =  auth.setPassword(req.body.password)
     req.body.password = password.hash
     req.body.salt = password.salt
 
@@ -52,16 +52,36 @@ users.get('/users/:userId', async (req, res) => {
     if (user) {
         res.send(user)
     } else {
-        res.status(404).send("User not be found")
+        res.status(404).send("User not found")
     }
 })
 
-class Middleware {
+// POST -- Authenticate credentials, return a token if correct
+users.post('/users/auth', async (req, res) => {
+    let incomingEmail = req.body.email
+    let incomingPassword = req.body.password
+
+    let user = data.find(item => {
+        return incomingEmail == item.email
+    })
+
+    if (user && auth.validatePassword(incomingPassword, user.salt, user.password)) {
+        // Placeholder token implementation
+        res.send({
+            uid: user.uid,
+            token: "supersecret"
+        })
+    } else {
+        res.status(404).send("Invalid credentials")
+    }
+})
+
+class Authentication {
     /**
      * Set a password. Returns a hash of that password and salt used for it.
      * @param {string} password A string of the password we want to hash
      * @returns {object} An object that contains a hash of the password and salt used for it.
-     * @memberof Middleware
+     * @memberof Authentication
      */
     setPassword(password){
         let salt = crypto.randomBytes(16).toString('hex')
@@ -78,7 +98,7 @@ class Middleware {
      * @param {string} salt A string used to reverse-hash the hash to get the original password value.
      * @param {string} hash A string that is a hash of the original password combined with salt.
      * @returns {boolean} A boolean on whether the password provided is valid.
-     * @memberof Middleware
+     * @memberof Authentication
      */
     validatePassword(password, salt, hash){
         const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512').toString('hex')
@@ -86,6 +106,6 @@ class Middleware {
     }
 }
 
-const middleware = new Middleware()
+const auth = new Authentication()
 
 module.exports = users
