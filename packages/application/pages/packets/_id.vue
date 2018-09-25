025e8b1628c62f19f776586de5c7161c599ffe63
Sebastian Guillema <sguillema@hotmail.com>
2018-09-25 18:27:45 +1000

    Data fetch and component mount methods set

---
+++ packages/application/pages/packets/_id.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title>
+          <h1>Packet {{$route.params.id}}</h1>
+        </v-card-title>
+        <v-card-text>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    middleware: 'authenticated',
+    data () {
+      return {
+        packet: null
+      }
+    },
+    methods: {
+      submit () {
+      },
+      clear () {
+      }
+    },
+    mounted () {
+      axios.get(`${process.env.apiUrl}/packets/${this.$route.params.id}`)
+        .then(res => {
+          this.packet = res.data
+        })
+    },
+    fetch ({store, route}) {
+      store.commit('updatePageTitle', `Packet ${route.params.id}`)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
